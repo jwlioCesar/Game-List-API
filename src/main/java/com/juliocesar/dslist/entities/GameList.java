72749131a6d1842d5edc923e4387b03c6803a094
Júlio César <juliocfds20@gmail.com>
2023-10-17 10:27:56 -0300

    Database seed update

--- src/main/java/com/juliocesar/dslist/entities/GameList.java
+++ src/main/java/com/juliocesar/dslist/entities/GameList.java
@@ -3,6 +3,8 @@ package com.juliocesar.dslist.entities;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.Table;
 public class GameList {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
